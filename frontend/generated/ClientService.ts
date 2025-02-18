4b6a7da947b1d25ddd82265a96607fd36500b4d8
大连光哥 <xgwangdl@163.com>
2025-02-18 21:39:14 +0800

    Delete frontend/generated directory

--- frontend/generated/ClientService.ts
+++
@@ -1,6 +0,0 @@
-import { EndpointRequestInit as EndpointRequestInit_1 } from "@vaadin/hilla-frontend";
-import type InterViewRecord_1 from "./com/guangge/Interview/test/WrittenTestTools/InterViewRecord.js";
-import client_1 from "./connect-client.default.js";
-async function getInterView_1(init?: EndpointRequestInit_1): Promise<Array<InterViewRecord_1>> { return client_1.call("ClientService", "getInterView", {}, init); }
-async function sendMail_1(name: string, init?: EndpointRequestInit_1): Promise<void> { return client_1.call("ClientService", "sendMail", { name }, init); }
-export { getInterView_1 as getInterView, sendMail_1 as sendMail };
--- frontend/generated/endpoints.ts
+++
@@ -1,3 +0,0 @@
-import * as AssistantService_1 from "./AssistantService.js";
-import * as ClientService_1 from "./ClientService.js";
-export { AssistantService_1 as AssistantService, ClientService_1 as ClientService };
