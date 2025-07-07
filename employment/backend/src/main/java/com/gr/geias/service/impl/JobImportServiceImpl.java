51ffe9df7fa23687f9ac1b8aef2457784c34c4db
closedAquarius <2383731319@qq.com>
2025-07-07 21:15:31 +0800

    部分数字大屏的后端

--- employment/backend/src/main/java/com/gr/geias/service/impl/JobImportServiceImpl.java
+++ employment/backend/src/main/java/com/gr/geias/service/impl/JobImportServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class JobImportServiceImpl implements JobImportService {
@@ -40,5 +41,11 @@ public class JobImportServiceImpl implements JobImportService {
                 mapper.batchInsert(list);
             }
         }
+    }
+
+
+    @Override
+    public List<Map<String, Object>> getTopPreferredMajors() {
+        return mapper.getTopPreferredMajors();
     }
 }
